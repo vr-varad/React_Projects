2dbac9eafc4d767a0b3f7fbfc12f161a94da7396
Varad Gupta <varadgupta21@gmail.com>
2023-12-27 00:24:04 +0530

    Added 6th and 7th project

---
+++ 7-slider/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people,setPeople] = useState(data)
+  const [index,setIndex] = useState(0)
+  return <section className='section'>
+    <div className='title'>
+      <h2>
+        <span>/</span>Reviews
+      </h2>
+    </div>
+    <div className='section-center'>
+      {people.map((person,index)=>{
+        const {id,image,name,title,quote} = person
+        //more stifff
+        return <article key={id}>
+          <img src={image} alt={name} className='person-img'></img>
+          <h4>{name}</h4>
+          <p className='title'>{title}</p>
+          <p className='text'>{quote}</p>
+          <FaQuoteRight className='icon'></FaQuoteRight>
+        </article>
+      })}
+      <button className='prev'><FiChevronLeft></FiChevronLeft></button>
+      <button className='next'><FiChevronRight></FiChevronRight></button>
+    </div>
+  </section>
+}
+
+export default App;
